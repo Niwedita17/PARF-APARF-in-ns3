2a8936e459a11c2773c9a4f35fb1d8ff38cc5acc
Niwedita17 <niwedita.dakshana2017@gmail.com>
2020-11-01 04:02:52 +0530

    sumo integration with ns3

---
+++ ns-3.28.1/build/build-status.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+# Programs that are runnable.
+ns3_runnable_programs = ['build/scratch/ns3.28.1-scratch-simulator-debug', 'build/scratch/ns3.28.1-vanet-routing-compare-debug', 'build/scratch/subdir/ns3.28.1-subdir-debug']
+
+# Scripts that are runnable.
+ns3_runnable_scripts = []
+
